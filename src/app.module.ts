ace709a26a969a47226f39972ff76143944f4333
박상후 <sksk8922@gmail.com>
2023-08-24 23:20:22 +0900

    [신규] Auth resource 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
@@ -14,6 +15,7 @@ import { UserModule } from './user/user.module';
             synchronize: true,
         }),
         UserModule,
+        AuthModule,
     ],
     providers: [],
 })
