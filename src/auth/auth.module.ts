8a0bf6e9bb6ac8e958a44857cd6e814b641d61ee
박상후 <sksk8922@gmail.com>
2023-08-25 23:01:07 +0900

    [신규] passport-jwt 구현

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+    imports: [UserModule, PassportModule],
+    controllers: [AuthController],
+    providers: [AuthService],
 })
 export class AuthModule {}
