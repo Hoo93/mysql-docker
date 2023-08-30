bfe1ded82575802bbee21d028c2ab9041720de0e
박상후 <sksk8922@gmail.com>
2023-08-30 23:58:58 +0900

    [신규] user update 구현

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dto/createUser.dto';
 import { UpdateUserDto } from './dto/updateUser.dto';
 import { UserRepository } from './user.repository';
 import { User } from './entities/user.entity';
+import { UpdateResult } from 'typeorm';
 
 @Injectable()
 export class UserService {
@@ -27,5 +28,19 @@ export class UserService {
             throw new NotFoundException("There's no user");
         }
         return users;
+    }
+
+    async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+        const user: User = await this.getUserById(id);
+
+        const updateResult: UpdateResult = await this.userRepository.update(
+            { id: id },
+            updateUserDto,
+        );
+        if (updateResult.affected === 0) {
+            throw new BadRequestException('update failed');
+        }
+
+        return await this.getUserById(id);
     }
 }
