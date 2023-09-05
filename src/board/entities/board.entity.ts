488402fa40f98252efebc2860c3102b8c3533ba0
박상후 <sksk8922@gmail.com>
2023-09-05 23:58:25 +0900

    [추가] Board Entity 생성

--- src/board/entities/board.entity.ts
+++ src/board/entities/board.entity.ts
@@ -1,1 +1,17 @@
-export class Board {}
+import { ApiProperty } from '@nestjs/swagger';
+import { BaseTimeEntity } from 'src/BaseTimeEntity';
+import { Column } from 'typeorm';
+
+export class Board extends BaseTimeEntity {
+    @ApiProperty()
+    @Column()
+    title: string;
+
+    @ApiProperty()
+    @Column()
+    description: string;
+
+    @ApiProperty()
+    @Column()
+    category: string;
+}
