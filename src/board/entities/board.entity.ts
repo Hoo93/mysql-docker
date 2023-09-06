f2b82e4b948f35868ac27679b76fa2844ff67463
박상후 <sksk8922@gmail.com>
2023-09-06 23:13:03 +0900

    [추가] Board Entity swagger 설명 추가

--- src/board/entities/board.entity.ts
+++ src/board/entities/board.entity.ts
@@ -3,15 +3,23 @@ import { BaseTimeEntity } from 'src/BaseTimeEntity';
 import { Column } from 'typeorm';
 
 export class Board extends BaseTimeEntity {
-    @ApiProperty()
+    @ApiProperty({ description: '글 제목', example: 'board title', minLength: 1, maxLength: 50 })
     @Column()
     title: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        description: '글 내용',
+        example: 'this is board description',
+        minLength: 1,
+        maxLength: 50,
+    })
     @Column()
     description: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        description: 'this is foreign key of category',
+        example: 'board title',
+    })
     @Column()
-    category: string;
+    categoryCode: string;
 }
