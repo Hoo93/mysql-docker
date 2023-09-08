0ea719c3f34448d6e44fcb5214425ac40e8bebc8
박상후 <sksk8922@gmail.com>
2023-09-08 21:27:30 +0900

    [수정] Board Service async 수정

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -4,23 +4,23 @@ import { UpdateBoardDto } from './dto/update-board.dto';
 
 @Injectable()
 export class BoardService {
-  create(createBoardDto: CreateBoardDto) {
-    return 'This action adds a new board';
-  }
+    async create(createBoardDto: CreateBoardDto) {
+        return 'This action adds a new board';
+    }
 
-  findAll() {
-    return `This action returns all board`;
-  }
+    async findAll() {
+        return `This action returns all board`;
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} board`;
-  }
+    async findOne(id: number) {
+        return `This action returns a #${id} board`;
+    }
 
-  update(id: number, updateBoardDto: UpdateBoardDto) {
-    return `This action updates a #${id} board`;
-  }
+    async update(id: number, updateBoardDto: UpdateBoardDto) {
+        return `This action updates a #${id} board`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} board`;
-  }
+    async remove(id: number) {
+        return `This action removes a #${id} board`;
+    }
 }
