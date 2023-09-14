9986acc7eb94ade116b2dc48d82a9d67f6cc74f7
박상후 <sksk8922@gmail.com>
2023-09-14 23:41:36 +0900

    [추가] Attendance resource 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmOption } from './dbms/typeorm.config';
 import { BoardModule } from './board/board.module';
 import { DataSource } from 'typeorm';
+import { AttendanceModule } from './attendance/attendance.module';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(TypeOrmOption), UserModule, AuthModule, BoardModule],
+    imports: [TypeOrmModule.forRoot(TypeOrmOption), UserModule, AuthModule, BoardModule, AttendanceModule],
 })
 export class AppModule {
     constructor(private dataSource: DataSource) {}
