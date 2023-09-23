01f4b3a71913640b3f19c3ffec310176c9d48b51
박상후 <sksk8922@gmail.com>
2023-09-23 23:59:32 +0900

    [수정] module app 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmOption } from './dbms/typeorm.config';
-import { BoardModule } from './board/board.module';
 import { DataSource } from 'typeorm';
 import { AttendanceModule } from './attendance/attendance.module';
-import { ManagerModule } from './manager/manager.module';
 import { AttendeeModule } from './attendee/attendee.module';
 import { CommentModule } from './comment/comment.module';
 
@@ -15,9 +13,7 @@ import { CommentModule } from './comment/comment.module';
         TypeOrmModule.forRoot(TypeOrmOption),
         UserModule,
         AuthModule,
-        BoardModule,
         AttendanceModule,
-        ManagerModule,
         AttendeeModule,
         CommentModule,
     ],
