c201611b095da144cae53f4a64e645492873d1d4
박상후 <sksk8922@gmail.com>
2023-10-01 00:02:51 +0900

    [추가] swagger tag 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,12 +3,19 @@ import { AuthService } from './auth.service';
 import { UserCredentialDto } from './dto/user.credential.dto';
 import { CreateUserDto } from 'src/user/dto/createUser.dto';
 import { User } from 'src/user/entities/user.entity';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('/signin')
+    @ApiResponse({
+        status: 200,
+        description: 'user validate',
+        type: Object,
+    })
     async signin(@Body() userCredentialDto: UserCredentialDto): Promise<{ accessToken: string }> {
         try {
             return this.authService.validateUser(userCredentialDto);
@@ -18,6 +25,11 @@ export class AuthController {
     }
 
     @Post('/signup')
+    @ApiResponse({
+        status: 200,
+        description: 'user signup',
+        type: Number,
+    })
     async signup(@Body() createUserDto: CreateUserDto): Promise<Number> {
         try {
             return this.authService.signup(createUserDto);
