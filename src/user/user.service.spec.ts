bd23d64047bf380b744aa9ad170db2e3a5fabaaa
박상후 <sksk8922@gmail.com>
2023-10-02 23:58:47 +0900

    [추가] getUserById test 추가

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -4,6 +4,8 @@ import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
 import { TypeOrmOption } from 'src/dbms/typeorm.config';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { mock } from 'node:test';
+// import * as jest from 'jest';
 
 describe('UserService', () => {
     let userService: UserService;
@@ -25,8 +27,9 @@ describe('UserService', () => {
             ],
         }).compile();
 
-        userService = module.get<UserService>(UserService);
         userRepository = module.get<Repository<User>>(getRepositoryToken(User));
+        // userService = module.get<UserService>(UserService);
+        userService = new UserService(userRepository);
     });
 
     it('should be defined', () => {
@@ -44,8 +47,26 @@ describe('UserService', () => {
             );
             user.id = 1;
         });
+
         it('should be a function', () => {
             expect(typeof userService.getUserById).toBe('function');
+        });
+
+        it('ID 값으로 User를 조회한다.', async () => {
+            let userId = 2;
+            let user = User.signup(
+                'test name',
+                'test password',
+                'test email',
+                new Date(),
+                new Date(),
+            );
+            user.id = userId;
+
+            jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(user);
+
+            const foundUser = await userService.getUserById(userId);
+            expect(foundUser).toBe(user);
         });
     });
 });
