9500d2da4df892e41c00e1e2a5c9387829e8e568
박상후 <sksk8922@gmail.com>
2023-10-05 23:36:49 +0900

    [추가] attendanceRepository 의존성 주입

--- src/attendance/attendance.service.ts
+++ src/attendance/attendance.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAttendanceDto } from './dto/create-attendance.dto';
 import { UpdateAttendanceDto } from './dto/update-attendance.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Attendance } from './entities/attendance.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AttendanceService {
-  create(createAttendanceDto: CreateAttendanceDto) {
-    return 'This action adds a new attendance';
-  }
+    constructor(
+        @InjectRepository(Attendance)
+        private readonly attendanceRepository: Repository<Attendance>,
+    ) {}
 
-  findAll() {
-    return `This action returns all attendance`;
-  }
+    async create(createAttendanceDto: CreateAttendanceDto) {
+        return 'This action adds a new attendance';
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} attendance`;
-  }
+    findAll() {
+        return `This action returns all attendance`;
+    }
 
-  update(id: number, updateAttendanceDto: UpdateAttendanceDto) {
-    return `This action updates a #${id} attendance`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} attendance`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} attendance`;
-  }
+    update(id: number, updateAttendanceDto: UpdateAttendanceDto) {
+        return `This action updates a #${id} attendance`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} attendance`;
+    }
 }
