9fae342faf984d7966fb3aa6fe604a569651ca95
박상후 <sksk8922@gmail.com>
2023-10-14 23:29:07 +0900

    [추가] 단위테스트 설정 구현

--- src/attendance/attendance.service.spec.ts
+++ src/attendance/attendance.service.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AttendanceService } from './attendance.service';
+import { Attendance } from './entities/attendance.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 describe('AttendanceService', () => {
-  let service: AttendanceService;
+    let attendanceService: AttendanceService;
+    let attendanceRepository: Repository<Attendance>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AttendanceService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                AttendanceService,
+                {
+                    provide: getRepositoryToken(Attendance),
+                    useValue: {
+                        create: jest.fn(),
+                        save: jest.fn(),
+                        update: jest.fn(),
+                        findOneBy: jest.fn(),
+                    },
+                },
+            ],
+        }).compile();
 
-    service = module.get<AttendanceService>(AttendanceService);
-  });
+        attendanceRepository = module.get<Repository<Attendance>>(getRepositoryToken(Attendance));
+        attendanceService = new AttendanceService(attendanceRepository);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(attendanceService).toBeDefined();
+    });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -2,17 +2,41 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+    let service: AuthService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [AuthService],
+        }).compile();
 
-    service = module.get<AuthService>(AuthService);
-  });
+        service = module.get<AuthService>(AuthService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    let userService: UserService;
+    let userRepository: Repository<User>;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                UserService,
+                {
+                    provide: getRepositoryToken(User),
+                    useValue: {
+                        create: jest.fn(),
+                        save: jest.fn(),
+                        update: jest.fn(),
+                        findOneBy: jest.fn(),
+                    },
+                },
+            ],
+        }).compile();
+
+        userRepository = module.get<Repository<User>>(getRepositoryToken(User));
+        // userService = module.get<UserService>(UserService);
+        userService = new UserService(userRepository);
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
